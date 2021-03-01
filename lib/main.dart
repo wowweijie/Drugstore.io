7ebd911b67f7dbabe781d9b355d46e01110b9b5b
xiaoqing872 <61306326+xiaoqing872@users.noreply.github.com>
2021-03-01 18:00:57 +0800

    updated edit profile page again

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
 
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(debugShowCheckedModeBanner: false, home: NewProfilePage());
+          return MaterialApp(debugShowCheckedModeBanner: false, home: AppEntryPage());
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
